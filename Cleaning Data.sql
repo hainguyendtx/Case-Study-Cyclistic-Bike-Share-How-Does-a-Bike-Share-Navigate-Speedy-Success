6b5d0510294d4baa2d1e4e7f4fc65b1a4d694d4a
hainguyendtx <157367308+hainguyendtx@users.noreply.github.com>
2024-01-22 06:53:25 -0600

    Update Cleaning Data.sql

--- Cleaning Data.sql
+++ Cleaning Data.sql
@@ -1,4 +1,6 @@
---- Checking for empty values, there aren't any so we will move on
+--- Checking for null or duplicate values, remove if necassary
+
+--- Checking for null values
 SELECT *
 FROM dbo.cyclistic_biketrips_2023
 WHERE
@@ -10,7 +12,7 @@ OR start_station_name IS NULL
 OR ended_station_name IS NULL
 OR member_casual IS NULL
 
---- Checking for duplicate values, there aren't any so we will move on
+--- Checking for duplicate values
 SELECT *, COUNT(*)
 FROM dbo.cyclistic_biketrips_2023
 GROUP BY ride_id, rideable_type, started_at, ended_at, start_station_name, ended_station_name, member_casual
